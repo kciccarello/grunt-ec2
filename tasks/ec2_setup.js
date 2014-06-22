b0a967aed825f574c2c2004fed6794c098be95da
Kris Ciccarello <kciccarello@gmail.com>
2014-06-22 12:50:56 -0400

    Support for iptables-persistent

--- tasks/ec2_setup.js
+++ tasks/ec2_setup.js
@@ -26,54 +26,56 @@ module.exports = function (grunt) {
         var platform = conf('PM2_PLATFORM');
         var steps = [[
             util.format('echo "configuring up %s instance..."', name)
-        ], [ // enable forwarding
-            'cp /etc/sysctl.conf /tmp/',
-            'echo "net.ipv4.ip_forward = 1" >> /tmp/sysctl.conf',
-            'sudo cp /tmp/sysctl.conf /etc/',
-            'sudo sysctl -p /etc/sysctl.conf'
-        ], [ // forward port 80
-            forwardPort(80, 8080)
-        ], workflow.if_has('SSL_ENABLED', // forward port 443
-            forwardPort(443, 8433)
-        ), [ // rsync
-            util.format('sudo mkdir -p %s', versions),
-            util.format('sudo mkdir -p %s', cert),
-            util.format('sudo chown ubuntu %s', cert),
-            util.format('sudo mkdir -p %s', latest),
-            util.format('sudo chown ubuntu %s', latest)
-        ], workflow.if_has('SSL_ENABLED', { // send certificates
-            rsync: {
-                name: 'cert',
-                local: conf('SSL_CERTIFICATE_DIRECTORY'),
-                remote: conf('SRV_RSYNC_CERT'),
-                dest: conf('SRV_CERT'),
-                includes: [
-                    '*/',
-                    conf('SSL_CERTIFICATE'),
-                    conf('SSL_CERTIFICATE_KEY')
-                ],
-                excludes: ['*']
-            }
-        }), [ // node.js
+        ], [ // node.js
             'sudo apt-get install python-software-properties',
             'sudo add-apt-repository ppa:chris-lea/node.js -y',
             'sudo add-apt-repository ppa:chris-lea/nginx-devel -y',
             'sudo apt-get update',
-            'sudo apt-get install nodejs -y'
+            'sudo apt-get install nodejs -y',
+            'sudo apt-get install iptables-persistent'
         ],  [ // git
             'sudo apt-get install git -y'
         ],  [ // pm2
             'sudo apt-get install make g++ -y',
             'sudo npm install -g pm2',
             util.format('sudo pm2 startup %s', platform)
-        ]];
+          // enable forwarding
+          'cp /etc/sysctl.conf /tmp/',
+          'echo "net.ipv4.ip_forward = 1" >> /tmp/sysctl.conf',
+          'sudo cp /tmp/sysctl.conf /etc/',
+          'sudo sysctl -p /etc/sysctl.conf'
+        ], [ // forward port 80
+          forwardPort(80, 8080)
+        ], workflow.if_has('SSL_ENABLED', // forward port 443
+          forwardPort(443, 8433)
+        ), [ // rsync
+          util.format('sudo mkdir -p %s', versions),
+          util.format('sudo mkdir -p %s', cert),
+          util.format('sudo chown ubuntu %s', cert),
+          util.format('sudo mkdir -p %s', latest),
+          util.format('sudo chown ubuntu %s', latest)
+        ], workflow.if_has('SSL_ENABLED', { // send certificates
+          rsync: {
+            name: 'cert',
+            local: conf('SSL_CERTIFICATE_DIRECTORY'),
+            remote: conf('SRV_RSYNC_CERT'),
+            dest: conf('SRV_CERT'),
+            includes: [
+              '*/',
+              conf('SSL_CERTIFICATE'),
+              conf('SSL_CERTIFICATE_KEY')
+            ],
+            excludes: ['*']
+          }
+        })];
 
         function forwardPort(from, to) {
             return [
                 util.format('sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport %s -j REDIRECT --to-port %s', from, to),
                 util.format('sudo iptables -A INPUT -p tcp -m tcp --sport %s -j ACCEPT', from),
                 util.format('sudo iptables -A OUTPUT -p tcp -m tcp --dport %s -j ACCEPT', from),
-                'sudo iptables-save'
+                'sudo iptables-save',
+                'sudo iptables-save > /etc/iptables/rules.v4'
             ];
         }
 
