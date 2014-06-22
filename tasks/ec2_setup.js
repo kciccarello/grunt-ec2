4e4574b9456d26b7926c7985f21795a117fe451c
Kris Ciccarello <kciccarello@gmail.com>
2014-06-22 17:40:37 -0400

    cabal update before installing zip-archive

--- tasks/ec2_setup.js
+++ tasks/ec2_setup.js
@@ -84,8 +84,8 @@ module.exports = function (grunt) {
         function installPandoc() {
           return [
             'sudo apt-get install haskell-platform -y',
-            'cabal install zip-archive',
             'cabal update',
+            'cabal install zip-archive',
             'cabal install pandoc'
           ];
         }
